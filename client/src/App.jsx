4935efb39ec611bd8a4eacd6d5001b32b85fa7bc
Muhammad Shabbir <muhammadshabbir4589@gmail.com>
2025-10-19 20:33:45 +0500

    folder capalization fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -24,6 +24,7 @@ function App() {
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/reset-password" element={<ResetPassword />} />
         </Route>
+      
 
         {/* 🔹 Admin Routes */}
         <Route element={<DashboardLayout />}>
